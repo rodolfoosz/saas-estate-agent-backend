915f57fa4e1307ba02d0d4c72b51cbb42c60ed83
Rodolfo Oliveira <rodolfo@gmail.com>
2025-06-25 14:18:06 -0300

    adjust retorno

--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -21,8 +21,12 @@ export class AuthService {
 
   async login(user: any) {
     const payload = { email: user.email, sub: user.id };
+    const firstName = user.fullName?.split(' ')[0] || '';
     return {
       access_token: this.jwtService.sign(payload),
+      id: user.id,
+      email: user.email,
+      name: firstName,
     };
   }
 }
